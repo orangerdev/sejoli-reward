fa579e91f607332e5883cc8f50a485b27ba249d1
Ridwan Arifandi <hi@ridwanarifandi.com>
2020-09-30 11:01:07 +0700

    register several new hooks

--- includes/class-sejoli-reward.php
+++ includes/class-sejoli-reward.php
@@ -198,6 +198,7 @@ class Sejoli_Reward {
 
 		$this->loader->add_action( 'sejoli/database/setup',			$admin, 'register_database', 1);
 		$this->loader->add_action( 'admin_bar_menu',				$admin, 'add_point_link',	 12222);
+		$this->loader->add_action( 'admin_enqueue_scripts',			$admin, 'enqueue_css_js_scripts', 1099);
 
 		$json  = new Sejoli_Reward\Admin\Json( $this->get_plugin_name(), $this->get_version() );
 
@@ -234,21 +235,23 @@ class Sejoli_Reward {
 
 		$reward  = new Sejoli_Reward\Admin\Reward( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'init',							$reward, 'register_post_type', 	1222);
-		$this->loader->add_action( 'carbon_fields_register_fields',	$reward, 'setup_reward_fields', 1222);
-		$this->loader->add_filter( 'manage_posts_columns',			$reward, 'modify_post_columns',	1222, 2);
-		$this->loader->add_action( 'manage_posts_custom_column',	$reward, 'display_data_in_post_columns', 1222, 2);
-		$this->loader->add_filter( 'sejoli/product/meta-data',		$reward, 'set_product_point',		122);
-		$this->loader->add_action( 'admin_menu',					$reward, 'add_custom_point_menu', 	122);
-		$this->loader->add_filter( 'sejoli/admin/js-localize-data', $reward, 'set_localize_js_vars',	12);
-		$this->loader->add_filter( 'sejoli/admin/is-sejoli-page',	$reward, 'is_current_page_sejoli_page', 1222);
+		$this->loader->add_action( 'init',									$reward, 'register_post_type', 		1222);
+		$this->loader->add_action( 'carbon_fields_register_fields',			$reward, 'setup_reward_fields', 	1222);
+		$this->loader->add_action( 'wp_ajax_add-input-point-data',			$reward, 'add_manual_input_data',	1222);
+		$this->loader->add_action( 'admin_notices',							$reward, 'display_notice',			1222);
+		$this->loader->add_filter( 'manage_posts_columns',					$reward, 'modify_post_columns',		1222, 2);
+		$this->loader->add_action( 'manage_posts_custom_column',			$reward, 'display_data_in_post_columns', 1222, 2);
+		$this->loader->add_filter( 'sejoli/product/meta-data',				$reward, 'set_product_point',		122);
+		$this->loader->add_action( 'admin_menu',							$reward, 'add_custom_point_menu', 	122);
+		$this->loader->add_filter( 'sejoli/admin/js-localize-data', 		$reward, 'set_localize_js_vars',	12);
+		$this->loader->add_filter( 'sejoli/admin/is-sejoli-page',			$reward, 'is_current_page_sejoli_page', 1222);
 
 		$this->loader->add_filter( 'sejoli/notification/fields',    		$reward, 'set_notification_fields', 120);
 		$this->loader->add_filter( 'sejoli/product/fields',					$reward, 'set_product_fields',		12);
 		$this->loader->add_filter( 'sejoli/user-group/fields',				$reward, 'set_user_group_fields', 	12);
 		$this->loader->add_filter( 'sejoli/user-group/per-product/fields',	$reward, 'set_user_group_per_product_fields', 12, 2);
-		$this->loader->add_filter( 'sejoli/product/commission/fields',		$reward, 'set_commission_fields', 12);
-		$this->loader->add_filter( 'sejoli/user-group/detail',				$reward, 'set_user_group_detail', 12, 4);
+		$this->loader->add_filter( 'sejoli/product/commission/fields',		$reward, 'set_commission_fields', 	12);
+		$this->loader->add_filter( 'sejoli/user-group/detail',				$reward, 'set_user_group_detail', 	12, 4);
 		$this->loader->add_filter( 'sejoli/user-group/per-product/detail',	$reward, 'set_user_group_per_product_detail', 12, 2);
 
 	}
